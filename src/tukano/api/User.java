35d1f3bd6477cd30b5f8c9c704db2423a3eefbcc
Dan Ciubuc <dan.ciubuc1223@gmail.com>
2024-03-24 17:59:30 +0000

    testing git

--- src/tukano/api/User.java
+++ src/tukano/api/User.java
@@ -1,14 +1,16 @@
 package tukano.api;
 
 public class User {
-	
+
 	private String pwd;
 	private String email;
 	private String userId;
 	private String displayName;
 
-	public User() {}
-	
+	// hello goodbye holla pls wrod
+	public User() {
+	}
+
 	public User(String userId, String pwd, String email, String displayName) {
 		this.pwd = pwd;
 		this.email = email;
@@ -19,44 +21,51 @@ public class User {
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getPwd() {
 		return pwd;
 	}
+
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getDisplayName() {
 		return displayName;
 	}
+
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
 	}
-	
+
 	public String userId() {
 		return userId;
 	}
-	
+
 	public String pwd() {
 		return pwd;
 	}
-	
+
 	public String email() {
 		return email;
 	}
-	
+
 	public String displayName() {
 		return displayName;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", pwd=" + pwd + ", email=" + email + ", displayName=" + displayName + "]";
