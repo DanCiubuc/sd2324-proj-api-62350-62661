a63313712d7cb4f246f2dd29b94a057373a26c39
Dan Ciubuc <dan.ciubuc1223@gmail.com>
2024-03-25 11:12:44 +0000

    Added files related to the Shorts service

---
+++ src/tukano/servers/rest/RestShortsResouce.java
@@ -0,0 +1,64 @@
+package tukano.servers.rest;
+
+import java.util.List;
+
+import tukano.api.Short;
+import tukano.api.rest.RestShorts;
+
+public class RestShortsResouce implements RestShorts {
+
+    @Override
+    public Short createShort(String userId, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createShort'");
+    }
+
+    @Override
+    public void deleteShort(String shortId, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteShort'");
+    }
+
+    @Override
+    public Short getShort(String shortId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getShort'");
+    }
+
+    @Override
+    public List<String> getShorts(String userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getShorts'");
+    }
+
+    @Override
+    public void follow(String userId1, String userId2, boolean isFollowing, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'follow'");
+    }
+
+    @Override
+    public List<String> followers(String userId, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'followers'");
+    }
+
+    @Override
+    public void like(String shortId, String userId, boolean isLiked, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'like'");
+    }
+
+    @Override
+    public List<String> likes(String shortId, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'likes'");
+    }
+
+    @Override
+    public List<String> getFeed(String userId, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFeed'");
+    }
+
+}
